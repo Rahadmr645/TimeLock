92a03ed6ca6cbf3a1b532036ea73f7978a1d407c
Rahad <rahadmr645@gmail.com>
2025-04-21 03:25:14 +0300

    adding a routs

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/create', protect, async (req, res) => {
     try {
 
-        const { title,startTime,endTime } = req.body;
+        const { title, startTime, endTime } = req.body;
 
         if (!title) return res.status(400).json({ message: "please fill all the input" });
 
@@ -16,11 +16,27 @@ router.post('/create', protect, async (req, res) => {
             userId: req.user.id,
             startTime,
             endTime
-        
+
         });
 
         const saveTask = await newTask.save();
-        res.status(200).json({ message: `task create success full well done ${req.user.name}`,task: saveTask });
+        res.status(200).json({ message: `task create success full well done ${req.user.name}`, task: saveTask });
+    } catch (error) {
+        res.status(500).json({ message: "internal error", error: error.message })
+    }
+});
+
+
+
+
+
+// 02: get all your all the task
+router.get('/myTask', protect, async (req, res) => {
+    try {
+        const task = await Task.find({ userId: req.user.id });
+        if (!task) return res.status(400).json({ message: "Opps! yet no task" });
+
+        res.status(200).json({ message: `Here is your task ${req.user.name} `, task });
     } catch (error) {
         res.status(500).json({ message: "internal error", error: error.message })
     }
